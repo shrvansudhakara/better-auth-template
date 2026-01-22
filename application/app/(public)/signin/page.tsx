fa0cdc93d90ffd994bb0e4fa72f8c477b911cbd2
shrvansudhakara <shrvansudhakara@gmail.com>
2026-01-22 16:28:43 +0530

    refactor: remove unnecessary authClient session usage

--- application/app/(public)/signin/page.tsx
+++ application/app/(public)/signin/page.tsx
@@ -1,10 +1,8 @@
 "use client";
+
 import SignIn from "@/components/auth/sign-in";
-import { authClient } from "@/lib/auth/auth-client";
 
 export default function Home() {
-  const session = authClient.useSession();
-  console.log(session);
   return (
     <div className="flex min-h-screen items-center justify-center">
       <div className="w-full max-w-md">
